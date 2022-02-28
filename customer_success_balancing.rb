21f7be73c9f760f7750101079266cbdd007d0302
Davi Luis <daviluis321@gmail.com>
2022-02-28 18:34:30 -0300

    fix: adjust rubocop offenses

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require 'timeout'
 
@@ -13,21 +15,32 @@ class CustomerSuccessBalancing
     customer_success = if @away_customer_success.empty?
                          @customer_success
                        else
-                         # customer_success_active
-                         customer_success = @customer_success.select do |cs|
-                           !@away_customer_success.include? cs[:id]
-                         end
+                         customer_success_active
                        end
 
     customer_success.sort_by! { |cs| cs[:score] }
 
+    search_customers_by_cs(customer_success)
+  end
+
+  def customer_success_active
+    @customer_success.reject do |cs|
+      @away_customer_success.include? cs[:id]
+    end
+  end
+
+  def search_customers_by_cs(customer_success)
     number_customers_by_cs = Hash.new(0)
 
     @customers.each do |customer|
-      cs = customer_success.bsearch { |cs| cs[:score] >= customer[:score] }
-      number_customers_by_cs[cs[:id]] += 1 unless cs.nil?
+      cs_search = customer_success.bsearch { |cs| cs[:score] >= customer[:score] }
+      number_customers_by_cs[cs_search[:id]] += 1 unless cs_search.nil?
     end
 
+    verify_number_customers_by_cs
+  end
+
+  def verify_number_customers_by_cs
     return 0 if number_customers_by_cs.values.size != 1 && number_customers_by_cs.values.uniq.size == 1
 
     if !number_customers_by_cs.empty?
@@ -36,8 +49,4 @@ class CustomerSuccessBalancing
       0
     end
   end
-
-  # def customer_success_active
-  #  next
-  # end
 end
