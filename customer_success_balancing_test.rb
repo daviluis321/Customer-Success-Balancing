aac8045bcc09a718038062969658776cb67ac5d4
Davi Luis <daviluis321@gmail.com>
2022-02-28 17:38:35 -0300

    fix: adjust tests pipeline

---
+++ customer_success_balancing_test.rb
@@ -0,0 +1,78 @@
+require "minitest/autorun"
+require "timeout"
+require_relative "customer_success_balancing"
+
+class CustomerSuccessBalancingTests < Minitest::Test
+
+    def test_scenario_one
+      balancer = CustomerSuccessBalancing.new(
+        build_scores([60, 20, 95, 75]),
+        build_scores([90, 20, 70, 40, 60, 10]),
+        [2, 4]
+      )
+      assert_equal 1, balancer.execute
+    end
+
+    def test_scenario_two
+      balancer = CustomerSuccessBalancing.new(
+        build_scores([11, 21, 31, 3, 4, 5]),
+        build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+        []
+      )
+      assert_equal 0, balancer.execute
+    end
+
+    def test_scenario_three
+      balancer = CustomerSuccessBalancing.new(
+        build_scores(Array(1..999)),
+        build_scores(Array.new(10000, 998)),
+        [999]
+      )
+      result = Timeout.timeout(1.0) { balancer.execute }
+      assert_equal 998, result
+    end
+
+    def test_scenario_four
+      balancer = CustomerSuccessBalancing.new(
+        build_scores([1, 2, 3, 4, 5, 6]),
+        build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+        []
+      )
+      assert_equal 0, balancer.execute
+    end
+
+    def test_scenario_five
+      balancer = CustomerSuccessBalancing.new(
+        build_scores([100, 2, 3, 3, 4, 5]),
+        build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+        []
+      )
+      assert_equal 1, balancer.execute
+    end
+
+    def test_scenario_six
+      balancer = CustomerSuccessBalancing.new(
+        build_scores([100, 99, 88, 3, 4, 5]),
+        build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+        [1, 3, 2]
+      )
+      assert_equal 0, balancer.execute
+    end
+
+    def test_scenario_seven
+      balancer = CustomerSuccessBalancing.new(
+        build_scores([100, 99, 88, 3, 4, 5]),
+        build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+        [4, 5, 6]
+      )
+      assert_equal 3, balancer.execute
+    end
+
+    private
+
+    def build_scores(scores)
+      scores.map.with_index do |score, index|
+        { id: index + 1, score: score }
+      end
+    end
+  end
