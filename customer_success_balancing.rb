6c4deb563b4c140b0735329cd1945b428b6584b0
Davi Luis <daviluis321@gmail.com>
2022-03-03 04:42:48 -0300

    refactor: rename functions customer balancer

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -14,19 +14,20 @@ class CustomerSuccessBalancing
     available_customer_success = if @away_customer_success.empty?
                                    @customer_success
                                  else
-                                   customer_success_active
+                                   remove_away_customer_success
                                  end
 
     return 0 if available_customer_success.nil?
 
     sort_customer_success(available_customer_success)
 
-    search_customers_by_cs(available_customer_success)
+    number_customers_by_cs = search_number_customer_by_cs(available_customer_success)
+    get_popular_customer_sucess_id(number_customers_by_cs)
   end
 
   private
 
-  def customer_success_active
+  def remove_away_customer_success
     @customer_success.reject! do |cs|
       @away_customer_success.include? cs[:id]
     end
@@ -36,18 +37,18 @@ class CustomerSuccessBalancing
     customer_success.sort_by! { |cs| cs[:score] }
   end
 
-  def search_customers_by_cs(customer_success)
-    number_customers_by_cs = Hash.new(0)
+  def search_number_customer_by_cs(customer_success)
+    number_customer_by_cs = Hash.new(0)
 
     @customers.each do |customer|
       cs_search = customer_success.bsearch { |cs| cs[:score] >= customer[:score] }
-      number_customers_by_cs[cs_search[:id]] += 1 unless cs_search.nil?
+      number_customer_by_cs[cs_search[:id]] += 1 unless cs_search.nil?
     end
 
-    verify_number_customers_by_cs(number_customers_by_cs)
+    number_customer_by_cs
   end
 
-  def verify_number_customers_by_cs(number_customers_by_cs)
+  def get_popular_customer_sucess_id(number_customers_by_cs)
     cs_score = number_customers_by_cs.values
     return 0 if cs_score.size != 1 && cs_score.uniq.size == 1
 
